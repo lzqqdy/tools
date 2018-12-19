6e0b1c2e416900f72f8d16413b306daf74f4e04e
lzq <986499810@qq.com>
2018-12-19 14:03:41 +0800

    test

---
+++ src/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace lzqqdy\tools;
+/**
+ * Class Test
+ * @package lzqqdy\tools
+ */
+class Test
+{
+    /**
+     * @param $param
+     * @return mixed
+     */
+    public function get($param)
+    {
+        return $param;
+    }
+}
