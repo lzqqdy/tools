eabce15d4da5cdf2e6ff085859e784f49e5451d4
root <986499810@qq.com>
2019-03-18 11:40:19 +0800

    更新

--- src/Common.php
+++ src/Common.php
@@ -586,17 +586,32 @@ class Common
      * 生成随机颜色
      * @return string
      */
-    function randomColor() {
+    function randomColor()
+    {
         $str = '#';
-        for($i = 0 ; $i < 6 ; $i++) {
-            $randNum = rand(0 , 15);
-            switch ($randNum) {
-                case 10: $randNum = 'A'; break;
-                case 11: $randNum = 'B'; break;
-                case 12: $randNum = 'C'; break;
-                case 13: $randNum = 'D'; break;
-                case 14: $randNum = 'E'; break;
-                case 15: $randNum = 'F'; break;
+        for ($i = 0; $i < 6; $i++)
+        {
+            $randNum = rand(0, 15);
+            switch ($randNum)
+            {
+                case 10:
+                    $randNum = 'A';
+                    break;
+                case 11:
+                    $randNum = 'B';
+                    break;
+                case 12:
+                    $randNum = 'C';
+                    break;
+                case 13:
+                    $randNum = 'D';
+                    break;
+                case 14:
+                    $randNum = 'E';
+                    break;
+                case 15:
+                    $randNum = 'F';
+                    break;
             }
             $str .= $randNum;
         }
