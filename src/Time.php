07b3f212d7f09c3ef605368f343fa2362c333595
root <986499810@qq.com>
2019-05-07 18:12:09 +0800

    update

--- src/Time.php
+++ src/Time.php
@@ -8,6 +8,12 @@ namespace lzqqdy\tools;
  */
 class Time
 {
+    const YEAR = 31536000;
+    const MONTH = 2592000;
+    const WEEK = 604800;
+    const DAY = 86400;
+    const HOUR = 3600;
+    const MINUTE = 60;
 
     /**
      * php将时间处理成“xx时间前”
@@ -72,5 +78,114 @@ class Time
             $date[$i] = date($format, strtotime('+' . $i - 7 . ' days', $time));
         }
         return $date;
+    }
+
+    /**
+     * 计算两个时区间相差的时长,单位为秒
+     *
+     * $seconds = self::offset('America/Chicago', 'GMT');
+     *
+     * [!!] A list of time zones that PHP supports can be found at
+     * <http://php.net/timezones>.
+     *
+     * @param   string $remote timezone that to find the offset of
+     * @param   string $local timezone used as the baseline
+     * @param   mixed $now UNIX timestamp or date string
+     * @return  integer
+     */
+    public static function offset($remote, $local = NULL, $now = NULL)
+    {
+        if ($local === NULL)
+        {
+            // Use the default timezone
+            $local = date_default_timezone_get();
+        }
+        if (is_int($now))
+        {
+            // Convert the timestamp into a string
+            $now = date(DateTime::RFC2822, $now);
+        }
+        // Create timezone objects
+        $zone_remote = new DateTimeZone($remote);
+        $zone_local = new DateTimeZone($local);
+        // Create date objects from timezones
+        $time_remote = new DateTime($now, $zone_remote);
+        $time_local = new DateTime($now, $zone_local);
+        // Find the offset
+        $offset = $zone_remote->getOffset($time_remote) - $zone_local->getOffset($time_local);
+        return $offset;
+    }
+
+    /**
+     * 计算两个时间戳之间相差的时间
+     *
+     * $span = self::span(60, 182, 'minutes,seconds'); // array('minutes' => 2, 'seconds' => 2)
+     * $span = self::span(60, 182, 'minutes'); // 2
+     *
+     * @param   int $remote timestamp to find the span of
+     * @param   int $local timestamp to use as the baseline
+     * @param   string $output formatting string
+     * @return  string   when only a single output is requested
+     * @return  array    associative list of all outputs requested
+     * @from https://github.com/kohana/ohanzee-helpers/blob/master/src/Date.php
+     */
+    public static function span($remote, $local = NULL, $output = 'years,months,weeks,days,hours,minutes,seconds')
+    {
+        // Normalize output
+        $output = trim(strtolower((string)$output));
+        if (!$output)
+        {
+            // Invalid output
+            return FALSE;
+        }
+        // Array with the output formats
+        $output = preg_split('/[^a-z]+/', $output);
+        // Convert the list of outputs to an associative array
+        $output = array_combine($output, array_fill(0, count($output), 0));
+        // Make the output values into keys
+        extract(array_flip($output), EXTR_SKIP);
+        if ($local === NULL)
+        {
+            // Calculate the span from the current time
+            $local = time();
+        }
+        // Calculate timespan (seconds)
+        $timespan = abs($remote - $local);
+        if (isset($output['years']))
+        {
+            $timespan -= self::YEAR * ($output['years'] = (int)floor($timespan / self::YEAR));
+        }
+        if (isset($output['months']))
+        {
+            $timespan -= self::MONTH * ($output['months'] = (int)floor($timespan / self::MONTH));
+        }
+        if (isset($output['weeks']))
+        {
+            $timespan -= self::WEEK * ($output['weeks'] = (int)floor($timespan / self::WEEK));
+        }
+        if (isset($output['days']))
+        {
+            $timespan -= self::DAY * ($output['days'] = (int)floor($timespan / self::DAY));
+        }
+        if (isset($output['hours']))
+        {
+            $timespan -= self::HOUR * ($output['hours'] = (int)floor($timespan / self::HOUR));
+        }
+        if (isset($output['minutes']))
+        {
+            $timespan -= self::MINUTE * ($output['minutes'] = (int)floor($timespan / self::MINUTE));
+        }
+        // Seconds ago, 1
+        if (isset($output['seconds']))
+        {
+            $output['seconds'] = $timespan;
+        }
+        if (count($output) === 1)
+        {
+            // Only a single output was requested, return it
+            return array_pop($output);
+        }
+        // Return array
+        return $output;
     }
 }
