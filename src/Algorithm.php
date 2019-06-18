6c56cab827635a2ff25c8fe969d47dfe63f53152
lzqqdy <Lzxq9865007>
2019-06-18 15:14:23 +0800

    update

--- src/Algorithm.php
+++ src/Algorithm.php
@@ -20,31 +20,11 @@ class Algorithm
     // +----------------------------------------------------------------------
 
     /**
-     * 冒泡算法1
+     * 冒泡算法
      * @param $arr
      * @return mixed
      */
-    public function bubble_sort_1($arr)
-    {
-        $n = count($arr);
-        for ($i = 0; $i < $n - 1; $i++) {
-            for ($j = $i + 1; $j < $n; $j++) {
-                if ($arr[$j] < $arr[$i]) {
-                    $temp = $arr[$i];
-                    $arr[$i] = $arr[$j];
-                    $arr[$j] = $temp;
-                }
-            }
-        }
-        return $arr;
-    }
-
-    /**
-     * 冒泡算法2
-     * @param $arr
-     * @return mixed
-     */
-    public function bubble_sort_2($arr)
+    public function bubble_sort($arr)
     {
         $n = count($arr);
         for ($i = 0; $i < $n - 1; $i++) {
