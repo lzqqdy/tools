ecf08a53e81875da1f2ee2d61e2625f53714ab5a
root <986499810@qq.com>
2019-06-28 17:52:44 +0800

    update

--- src/Other.php
+++ src/Other.php
@@ -58,4 +58,31 @@ class Other
     {
         return gethostbyname($_SERVER["SERVER_NAME"]);
     }
+
+    /**
+     * 返回打印数组结构
+     * @param string $var 数组
+     * @param string $indent 缩进字符
+     * @return string
+     */
+    public static function var_export_short($var, $indent = "")
+    {
+        switch (gettype($var)) {
+            case "string":
+                return '"' . addcslashes($var, "\\\$\"\r\n\t\v\f") . '"';
+            case "array":
+                $indexed = array_keys($var) === range(0, count($var) - 1);
+                $r = [];
+                foreach ($var as $key => $value) {
+                    $r[] = "$indent    "
+                        . ($indexed ? "" : self::var_export_short($key) . " => ")
+                        . self::var_export_short($value, "$indent    ");
+                }
+                return "[\n" . implode(",\n", $r) . "\n" . $indent . "]";
+            case "boolean":
+                return $var ? "TRUE" : "FALSE";
+            default:
+                return var_export($var, true);
+        }
+    }
 }
