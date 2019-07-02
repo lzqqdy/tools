30ee16c4ddf893483f4ee12127e4a5483694587e
root <986499810@qq.com>
2019-07-02 16:24:35 +0800

    update

--- src/Other.php
+++ src/Other.php
@@ -85,4 +85,18 @@ class Other
                 return var_export($var, true);
         }
     }
+
+    /**
+     * 判断是否SSL协议
+     * @return bool
+     */
+    public static function is_ssl()
+    {
+        if (isset($_SERVER['HTTPS']) && ('1' == $_SERVER['HTTPS'] || 'on' == strtolower($_SERVER['HTTPS']))) {
+            return true;
+        } elseif (isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT'])) {
+            return true;
+        }
+        return false;
+    }
 }
