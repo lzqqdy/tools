83690a57b709d58b907e44d1e1af078352055ff5
lzqqdy <Lzxq9865007>
2020-02-01 11:14:31 +0800

    严格校验身份证信息方法

--- src/Validate.php
+++ src/Validate.php
@@ -76,6 +76,84 @@ class Validate
         }
         return false;
     }
-    //TODO 身份证号|密码强度|文件格式|URL|IP|邮编
+    //TODO 密码强度|文件格式|URL|IP|邮编
 
+    /**
+     * 严格校验身份证信息
+     * @param $id_card
+     * @return bool
+     */
+    public static function check_id_card($id_card)
+    {
+        if (strlen($id_card) == 18) {
+            return self::id_card_checksum18($id_card);
+        } elseif ((strlen($id_card) == 15)) {
+            $id_card = self::id_card_15to18($id_card);
+            return self::id_card_checksum18($id_card);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 将15位身份证升级到18位
+     * @param $id_card
+     * @return bool|string
+     */
+    private static function id_card_15to18($id_card)
+    {
+        if (strlen($id_card) != 15) {
+            return false;
+        } else {
+            // 如果身份证顺序码是996 997 998 999，这些是为百岁以上老人的特殊编码
+            if (array_search(substr($id_card, 12, 3), array('996', '997', '998', '999')) !== false) {
+                $id_card = substr($id_card, 0, 6) . '18' . substr($id_card, 6, 9);
+            } else {
+                $id_card = substr($id_card, 0, 6) . '19' . substr($id_card, 6, 9);
+            }
+        }
+        $id_card = $id_card . self::id_card_verify_number($id_card);
+        return $id_card;
+    }
+
+    /**
+     * 18位身份证校验码有效性检查
+     * @param $id_card
+     * @return bool
+     */
+    private static function id_card_checksum18($id_card)
+    {
+        if (strlen($id_card) != 18) {
+            return false;
+        }
+        $id_card_base = substr($id_card, 0, 17);
+        if (self::id_card_verify_number($id_card_base) != strtoupper(substr($id_card, 17, 1))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 计算身份证校验码，根据国家标准GB 11643-1999
+     * @param $id_card_base
+     * @return bool|mixed
+     */
+    private static function id_card_verify_number($id_card_base)
+    {
+        if (strlen($id_card_base) != 17) {
+            return false;
+        }
+        //加权因子
+        $factor = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+        //校验码对应值
+        $verify_number_list = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+        $checksum = 0;
+        for ($i = 0; $i < strlen($id_card_base); $i++) {
+            $checksum += substr($id_card_base, $i, 1) * $factor[$i];
+        }
+        $mod = $checksum % 11;
+        $verify_number = $verify_number_list[$mod];
+        return $verify_number;
+    }
 }
